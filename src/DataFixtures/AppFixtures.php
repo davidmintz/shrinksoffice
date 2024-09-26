9945070ae33278aa8f7b3032c1024d81f016fbda
david <david@davidmintz.org>
2024-09-26 19:18:18 -0400

    make creation of two-patient sessions work

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,6 +11,7 @@ use Doctrine\Persistence\ObjectManager;
 use App\Factory\ServiceFactory;
 use App\Entity\PersonType;
 use Faker\Core\DateTime;
+use function Zenstruck\Foundry\get;
 
 class AppFixtures extends Fixture
 {
@@ -19,19 +20,22 @@ class AppFixtures extends Fixture
     {
 
         $times = ServiceFactory::$times;
-
         PersonFactory::createMany(count($times),['active'=>true,'type'=>PersonType::PATIENT]);
-
+        // two people will only pay, not be patients
         PersonFactory::createMany(2,['active'=>true,'type'=>PersonType::PAYER]);
+
         $patients = PersonFactory::findBy(['type'=>PersonType::PATIENT]);
+
+        // randomly assign to some people a payer other than themselves
         $keys = array_rand($patients,6);
         $patients[$keys[0]]->_real()->setPayer($patients[$keys[1]]->_real());
         $patients[$keys[2]]->_real()->setPayer($patients[$keys[3]]->_real());
         $payers = PersonFactory::findBy(['type'=>PersonType::PAYER]);
         $patients[$keys[4]]->_real()->setPayer($payers[0]->_real());
         $patients[$keys[5]]->_real()->setPayer($payers[1]->_real());
-        $patients = PersonFactory::all();
 
+
+        $patients = PersonFactory::findBy(['active'=>true,'type'=>PersonType::PATIENT]);
         $when = \DateTime::createFromFormat('U',strtotime("-3 months"));
         // rewind to 1st of month
         $when->setDate($when->format('Y'),$when->format('m'),1);
@@ -43,6 +47,8 @@ class AppFixtures extends Fixture
             if ($when->format('D') === 'Sun') {
                 $when->add(new \DateInterval('P1D'));
             }
+
+            // assign patients to time slots
             $days_appointments = array_filter($times, fn($time) => substr($time,0,3) == $when->format('D'));
             foreach ($days_appointments  as $appointment) {
                 $appointment_date = \DateTime::createFromInterface($when);
@@ -50,16 +56,19 @@ class AppFixtures extends Fixture
                 $hr = (int)$m[1]; $mm = (int)$m[2];
                 $appointment_date->setTime($hr,$mm,);
                 //echo "appointment: ".$appointment_date->format('D Y-m-d H:i:s'),"\n";
-                $patient = array_pop($patients);
+                $patient = array_pop($patients)->_disableAutoRefresh();
+                //printf("DEBUG: patients count is now %d\n",count($patients));
+                // printf("DEBUG: \$patient is a %s\n", gettype($patient));
                 if (count($patients) === 0) {
-                    echo "DEBUG: last patient\n";
+                    // make one with two patients
                     $other_patient = PersonFactory::createOne(['active'=>true,'type'=>PersonType::PATIENT]);
                     $these_patients = [$patient,$other_patient];
-                    $fee = max([$patient->getFee(),$other_patient->getFee()]);
+                    $fee = 30000; // max([$patient->getFee(),$other_patient->getFee()]);
                 } else {
                     $these_patients = [$patient,];
                     $fee = $patient->getFee();
                 }
+
                 ServiceFactory::createOne([
                         'patients'=>$these_patients,
                         'time'=>$appointment_date,
@@ -71,11 +80,11 @@ class AppFixtures extends Fixture
             }
             $when->add(new \DateInterval('P1D'));
         }
+
         // and repeat...
         $sessions = ServiceFactory::all();
-        //$patients = PersonFactory::all();
         foreach ($sessions as $session) {
-            //$patient = array_pop($patients);
+
             $date = \DateTime::createFromInterface($session->getDate());
             for ($i = 0; $i <= 4; $i++) {
                 $date->add(new \DateInterval('P7D'));
@@ -83,11 +92,12 @@ class AppFixtures extends Fixture
                 $patients = $session->getPatients();
                 if (count($patients) > 1) {
                     $fee = 30000; // keep it simple
-                    $payer = array_rand($patients->toArray());
+                    $payer = $patients[array_rand($patients->toArray())];
                 } else {
                     $fee = $patients[0]->getFee();
                     $payer = $patients[0]->getPayer() ?? $patients[0];
                 }
+
                 ServiceFactory::createOne(
                     [
                         'patients'=>$patients,//$session->getPatients(),
@@ -117,7 +127,7 @@ class AppFixtures extends Fixture
             $payers[$service->getPayer()->getId()][] = $service;
         }
         foreach($payers as $payer_id => $sessions) {
-            $payer = PersonFactory::find($payer_id)->_disableAutoRefresh();
+            $payer = PersonFactory::find($payer_id); $payer->_disableAutoRefresh();
             InvoiceFactory::createOne([
                 'payer' => $payer,
                 'services' => $sessions,
