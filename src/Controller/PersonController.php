44da2694109cb8597deef1c0f76ae16e930e8aba
david <david@davidmintz.org>
2025-03-13 22:42:32 -0400

    start add() in PersonController

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -8,11 +8,17 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class PersonController extends AbstractController
 {
-    #[Route('/person', name: 'app_person')]
+    #[Route('/people', name: 'people_index')]
     public function index(): Response
     {
-        return $this->render('person/index.html.twig', [
+        return $this->render('people/index.html.twig', [
             'controller_name' => 'PersonController',
         ]);
+    }
+
+    #[Route('/people/add', name: 'people_add')]
+    public function add(): Response
+    {
+        return $this->render('people/form.html.twig', []);
     }
 }
