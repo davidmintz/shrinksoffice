ce0d0e12a0ddaf53665cce15038b1aac0fec5a17
david <david@davidmintz.org>
2025-04-07 23:15:06 -0400

    fiddle with person_controller status message styling

--- assets/controllers/person_controller.js
+++ assets/controllers/person_controller.js
@@ -46,7 +46,7 @@ export default class extends Controller {
                     this.formTarget.outerHTML = data;
                 } else {
                     // Success: Hide form, show success status message
-                    const css_class = 'btn btn-sm btn-outline-light mt-2';
+                    const css_class = 'btn btn-sm btn-success flex-grow-1 text-center';
                     // I guess right now we don't need:
                     //const what = this.entityTypeValue;
                     const name = data.name;
@@ -55,16 +55,22 @@ export default class extends Controller {
                     const basePath = '/people';
 
                     this.statusTarget.innerHTML =
-                       `<strong>${name}</strong> has been successfully ${verbiage}
-                        <a href="${basePath}/view/${id}" class="${css_class}">view</a>
-                        <a href="${basePath}/update/${id}" class="${css_class}">edit</a>
-                        <a href="${basePath}/add" class="${css_class}">add another</a>`;
+                       `<strong>${name}</strong> has been successfully ${verbiage}.
+                       <div class="d-flex justify-content-center gap-2 w-50 mx-auto">
+                            <!-- <div class="btn-group btn-group-sm mt-2" role="group"> -->
+                                <a href="${basePath}/view/${id}" class="${css_class}">view</a>
+                                <a href="${basePath}/update/${id}" class="${css_class}">edit</a>
+                                <a href="${basePath}/add" class="${css_class}">add another</a>
+                            <!-- </div> -->
+                        </div>`;
 
                     this.formTarget.classList.add("d-none");
                     this.statusTarget.classList.remove("d-none");
 
                 }
             })
-            .catch(error => console.error("Error submitting form:", error));
+            .catch(
+                error => { console.error("Error submitting form:", error) }
+            );
     }
 }
